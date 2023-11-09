491493e05c64c52f727098e4c54f3d98084f5cfd
K Stephan Torres <kevinstephantorres@gmail.com>
2023-11-09 16:47:29 -0300

    Se agrego la funcionalidad del punto 3

---
+++ js/punto03.js
@@ -0,0 +1,27 @@
+const acumulador = [0,0,0,0,0,0,0,0,0,0,0]
+let sumador=0
+
+for(let i=0;i<50;i++){
+    const dado1=Math.trunc(Math.random()*6+1)
+    const dado2=Math.trunc(Math.random()*6+1)
+    const suma= dado1+dado2
+    
+    acumulador[suma-2]++
+}
+
+document.write(`<table>
+    <tr>
+        <th>Suma 🎲🎲</th>
+        <th>Apariciones</th>
+    </tr>
+`)
+
+for(let i=2;i<=12;i++){
+    document.write(`<tr>`)
+    document.write(`<th>${i}</th>`)
+    document.write(`<th>${acumulador[i-2]}</th>`)
+    sumador+=acumulador[i-2]
+    document.write(`</tr>`)
+}
+console.log(sumador)
+document.write(`</table>`)
